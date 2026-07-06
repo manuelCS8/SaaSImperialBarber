d08b0d7e9f8db582ada21df0b0f5dfdc37daa0bd
OscarH10 <oscarinhernandez447@gmail.com>
2026-07-05 21:50:11 -0600

    feat: conectar rutas del endpoint de resend al servidor

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import clientRoutes from './client.routes'; 
 import statsRoutes from './stats.routes';   
+import integrationRoutes from './integration.routes'; // 👈 1. IMPORTA TU NUEVA RUTA
 
 const router = Router();
 
 router.use('/clients', clientRoutes);
 router.use('/stats', statsRoutes); 
+
+router.use('/integrations', integrationRoutes);
 
 export default router;
